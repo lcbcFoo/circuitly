0d54096e974c3f1901c88ca7cd4090b2ff31a6cd
lcbcfoo <lcbc.lucascastro@gmail.com>
2020-05-18 16:21:16 -0300

    Add splitter and concat blocks

--- circuitly/loader.js
+++ circuitly/loader.js
@@ -4,6 +4,8 @@ import { load as module_connection_load } from './blocks/module_connection/modul
 import { load as create_signal_load } from './blocks/create_signal/create_signal.js';
 import { load as signal_getter_load } from './blocks/signal_getter/signal_getter.js'
 import { load as connections_designer_load } from './blocks/connections_designer/connections_designer.js'
+import { load as splitter_loader } from './blocks/splitter/splitter.js'
+import { load as concat_loader } from './blocks/concat/concat.js'
 
 //////////////////////////////////////////////////////////////////////////////
 // Blockly setup
@@ -44,6 +46,9 @@ module_connection_load(workspace);
 create_signal_load(workspace);
 signal_getter_load(workspace);
 connections_designer_load(workspace);
+splitter_loader(workspace);
+concat_loader(workspace);
+
 
 // Load blocks to workspace
 Blockly.Xml.domToWorkspace(workspaceBlocks, workspace);
@@ -85,9 +90,43 @@ Blockly.Python['nand'] = function(block) {
     var value_y = Blockly.Python.valueToCode(block, 'in_Y', Blockly.Python.ORDER_ATOMIC);
     var value_z = Blockly.Python.valueToCode(block, 'out_Z', Blockly.Python.ORDER_ATOMIC);
     // TODO: Assemble Python into code variable.
-    var code = 'nand ' + '(' + value_z + ', ' + value_y + 
-        ', ' + value_x + ')\n';
+    var name = get_temp_name('nand');
+    var code = 'nand ' + name + '(' + value_z + ', ' + value_y + 
+        ', ' + value_x + ');\n';
     // TODO: Change ORDER_NONE to the correct strength.
+    return code;
+}
+
+
+
+Blockly.Blocks['teste'] = {
+   init: function() {
+       this.appendDummyInput()
+           .appendField("teste");
+       this.appendValueInput("tin")
+           .setCheck(null)
+           .appendField("in tin");
+       this.appendValueInput("tout")
+           .setCheck(null)
+           .appendField("out tout");
+       this.setInputsInline(true);
+       this.setPreviousStatement(true, "LOGIC");
+       this.setNextStatement(true, "LOGIC");
+       this.setColour(90);
+       this.setTooltip("teste");
+       this.setHelpUrl("");
+   }
+};
+
+Blockly.Python["teste"] = function(block) {
+    var block_name = get_temp_name("teste");
+    var code = "teste " + block_name + "(";
+    var val_tin = Blockly.Python.valueToCode(block, "tin", Blockly.Python.ORDER_ATOMIC);
+    code += val_tin;
+    code += ", "
+    var val_tout = Blockly.Python.valueToCode(block, "tout", Blockly.Python.ORDER_ATOMIC);
+    code += val_tout;
+    code += ");"
     return code;
 }
 
