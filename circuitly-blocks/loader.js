a3550655961a3fa8eb0949167ad6038ae4d4662d
lcbcfoo <lcbc.lucascastro@gmail.com>
2020-06-19 19:15:00 -0300

    Place holder for blockly theme

--- circuitly-blocks/loader.js
+++ circuitly-blocks/loader.js
@@ -39,7 +39,10 @@ var options = {
     oneBasedIndex : true
 };
 
+
 var workspace = Blockly.inject(blocklyDiv, options);
+// TODO: create a more suitable theme. Reference 
+// https://developers.google.com/blockly/guides/configure/web/themes
 workspace.setTheme(Blockly.Themes.Dark);
 
 // Load circuitly blocks
