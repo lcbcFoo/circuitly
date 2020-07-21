43c19dd1de97376687b9bdc200ae80dd8bfadb0d
lcbcfoo <lcbc.lucascastro@gmail.com>
2020-07-20 21:22:32 -0300

    Adjust build files

--- webpack.config.js
+++ webpack.config.js
@@ -46,7 +46,8 @@ module.exports = {
         extensions: [ '.tsx', '.ts', '.js' ],
     },
     devServer: {
-        port: 3000,
+        host: '0.0.0.0',
+        port: 80,
         open: true,
         proxy: {
             "/api": "http://localhost:8080"
