c39f156e5b7bcdd33479dfd060dac45a6d295908
Lucas Castro <lucas@lampiao.ic.unicamp.br>
2020-10-14 21:59:39 -0300

    Try to host in port 3000

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,7 @@ module.exports = {
     },
     devServer: {
         host: '0.0.0.0',
-        port: 80,
+        port: 3000,
         open: true,
         proxy: {
             "/api": "http://localhost:8080"
