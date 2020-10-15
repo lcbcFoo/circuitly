635507bc605dae0163fe6ab1d00c23e47c0d9fd6
Lucas Castro <lucas@lampiao.ic.unicamp.br>
2020-10-14 22:23:30 -0300

    Revert "Try to host in port 3000"
    This reverts commit c39f156e5b7bcdd33479dfd060dac45a6d295908.

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,7 @@ module.exports = {
     },
     devServer: {
         host: '0.0.0.0',
-        port: 3000,
+        port: 80,
         open: true,
         proxy: {
             "/api": "http://localhost:8080"
